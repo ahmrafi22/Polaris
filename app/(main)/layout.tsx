ad2530b84eb1864c5f3607f5ad97dc4db17c270a
Raf-codes <163875268+Raf-codes@users.noreply.github.com>
2024-12-30 23:45:33 +0600

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,41 +1,46 @@
 "use client"
 
 import { Spinner } from "@/components/spinners";
-import { useConvexAuth } from "convex/react";
+import { useConvexAuth, useQuery } from "convex/react";
 import { redirect } from "next/navigation";
 import { Navigation } from "./__components/Navigation";
 import { SearchCommand } from "@/components/search-command";
+import { api } from "@/convex/_generated/api";
+import { useEffect } from "react";
 
-
-
-const MainLayout =({
+const MainLayout = ({
     children
 }: {
     children: React.ReactNode;
-}) =>{
+}) => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+    const user = useQuery(api.users.getCurrentUser);
 
-    const {isAuthenticated, isLoading} = useConvexAuth();
+    useEffect(() => {
+        if (user?.isBanned) {
+            redirect("/banned");
+        }
+    }, [user]);
 
-    if (isLoading){
-        return(
+    if (isLoading) {
+        return (
             <div className="h-full flex items-center justify-center">
                 <Spinner size={"lg"}/>
             </div>
-        )
+        );
     }
 
-    if (!isAuthenticated){
-        return redirect("/")
+    if (!isAuthenticated) {
+        return redirect("/");
     }
 
-    return(
-        <div className="h-full flex  dark:bg-[#1F1F1F]">
+    return (
+        <div className="h-full flex dark:bg-[#1F1F1F]">
             <Navigation />
-            <main className="flex-1 h-full overflow-y-auto ">
+            <main className="flex-1 h-full overflow-y-auto">
                 <SearchCommand />
-            {children}
+                {children}
             </main>
-            
         </div>
     );
 }
