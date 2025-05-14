2993b309a6c5a4574fea51ba1ac18404df796611
Rafi <163875268+ahmrafi22@users.noreply.github.com>
2025-05-14 12:48:58 +0600

    fixed key props

--- app/(promotion)/_components/animation.tsx
+++ app/(promotion)/_components/animation.tsx
@@ -381,7 +381,10 @@ const TextRotate = forwardRef<TextRotateRef, TextRotateProps>(
                     const totalIndex = previousCharsCount + charIndex
                     const animationProps = getAnimationProps(totalIndex)
                     return (
-                      <span className={cn(elementLevelClassName)}>
+                      <span 
+                        key={`${wordIndex}-${charIndex}`} 
+                        className={cn(elementLevelClassName)}
+                      >
                         <motion.span
                           {...animationProps}
                           key={charIndex}
